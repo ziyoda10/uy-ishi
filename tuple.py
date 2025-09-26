29069ce684bef0bf7977ee6d6d5087ffe4fdce1d
oktamovna <oktamovnaziyoda@gmail.com>
2025-09-26 17:32:45 +0500

    finish homework

---
+++ tuple.py
@@ -0,0 +1,25 @@
+fruits=("watermelon","melon","cherry")
+print(tuple)
+print(fruits[1])
+
+
+
+# numbers=(10,20,30,40,50)
+# print(len(numbers))
+# print(3 in numbers)
+# print(10 in numbers)
+
+# students ={
+#     1:"ali",
+#     2:"amirxon",
+#     3:"bobur",
+#     4:"ziyoda",
+# }
+# print(students)
+# students ={
+#     "001":{"name":"ali","age":14},
+#     "002":{"name":"amirxon","age":16}
+# }
+# print(students)
+
+print("Hello world")
